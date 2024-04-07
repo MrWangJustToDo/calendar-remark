92d28abfe7ed9ae6adf57cd3ef49247026179f24
MrWangJustToDo <2711470541@qq.com>
2024-04-07 21:15:39 +0800

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,48 +1,50 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 import react from '@vitejs/plugin-react-swc';
-import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
-import gzipPlugin from 'rollup-plugin-gzip';
-import path from 'path';
-import checker from 'vite-plugin-checker';
+// waiting this pr merge https://github.com/gpbl/react-day-picker/pull/2076
+// import react from "@my-react/react-vite";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
+import gzipPlugin from "rollup-plugin-gzip";
+import path from "path";
+import checker from "vite-plugin-checker";
 
 const pwaOptions: Partial<VitePWAOptions> = {
-  mode: 'development',
-  registerType: 'prompt',
+  mode: "development",
+  registerType: "prompt",
   devOptions: {
     enabled: true,
   },
-  includeAssets: ['favicon.svg'],
+  includeAssets: ["favicon.svg"],
   manifest: {
-    name: 'Calendar Remark',
-    short_name: 'Calendar Remark',
-    theme_color: '#ffffff',
-    description: 'A small and clean calendar app',
+    name: "Calendar Remark",
+    short_name: "Calendar Remark",
+    theme_color: "#ffffff",
+    description: "A small and clean calendar app",
     icons: [
       {
-        src: '/pwa-64x64.png',
-        sizes: '192x192',
-        type: 'image/png',
+        src: "/pwa-64x64.png",
+        sizes: "192x192",
+        type: "image/png",
       },
       {
-        src: '/pwa-192x192.png',
-        sizes: '192x192',
-        type: 'image/png',
+        src: "/pwa-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
       },
       {
-        src: '/pwa-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
+        src: "/pwa-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
       },
       {
-        src: '/apple-touch-icon-180x180.png',
-        sizes: '180x180',
-        type: 'image/png',
+        src: "/apple-touch-icon-180x180.png",
+        sizes: "180x180",
+        type: "image/png",
       },
       {
-        src: '/maskable-icon-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
-        purpose: 'maskable',
+        src: "/maskable-icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
       },
     ],
   },
@@ -58,7 +60,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
